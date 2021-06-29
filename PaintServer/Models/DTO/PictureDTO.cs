3bb82e26b4445034202fc13e8259665b543080b0
RockTF <egorov2001egorr@gmail.com>
2021-06-29 23:48:17 +0300

    update PictureDTO

--- PaintServer/Models/DTO/PictureDTO.cs
+++ PaintServer/Models/DTO/PictureDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PaintServer.Models.DTO
 {
@@ -7,17 +8,32 @@ namespace PaintServer.Models.DTO
         public int Id { get; set; }
         public int UserId { get; set; }
         public string PictureName { get; set; }
-        public string Picture { get; set; }
+        public string PictureContent { get; set; }
         public DateTime CreationDate { get; set; }
         public EPictureTypes PictureType { get; set; }
-        public int Curve { get; set; }
-        public int Dot { get; set; }
-        public int Ellipse { get; set; }
-        public int Line { get; set; }
-        public int Triangle { get; set; }
-        public int Polygon { get; set; }
-        public int Rectangle { get; set; }
-        public int SmoothCurve { get; set; }
-        public int RoundedRectangle { get; set; }
+        public List<KeyValuePair<string, int>> Dictionary { get; set; }
+
+        public PictureDTO()
+        {
+
+        }
+
+        public void FillFigures(Dictionary<string, int> values)
+        {
+            for (int i = 0; i < Dictionary.Count; i++)
+            {
+                var asd = values.GetValueOrDefault(Dictionary[i].Key);
+                Dictionary[i] = new KeyValuePair<string, int>(Dictionary[i].Key, Dictionary[i].Value + asd);
+            }
+        }
+
+        public void FillFiguresasdf()
+        {
+            var asdf = new List<string>();
+            for (int i = 0; i < Dictionary.Count; i++)
+            {
+                if (Dictionary[i].Value >= 5) asdf.Add(Dictionary[i].Key);
+            }
+        }
     }
 }
